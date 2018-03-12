6543686da22442672450538380127ee465c5cd72
radoslawlandowski <radoslaw.landowski@gmail.com>
2018-03-12 18:09:30 +0100

    Add test command to npm

--- spec/test-reporter.spec.js
+++ spec/test-reporter.spec.js
@@ -1,5 +1,13 @@
-describe("A suite is just a function", function() {
-    var a;
+describe("Test reporter tests", function() {
+    describe("Test reporter tests", function() {
+        var a;
+      
+        it("and so is a spec", function() {
+          a = true;
+      
+          expect(a).toBe(true);
+        });
+      });
   
     it("and so is a spec", function() {
       a = true;
