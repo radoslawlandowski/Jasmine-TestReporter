3760299873a0980dc8d297627a16e3cae08acb5b
radoslawlandowski <radoslaw.landowski@gmail.com>
2018-03-24 17:00:52 +0100

    Final commit for base stuff

--- dummy-project/spec/conf.js
+++ dummy-project/spec/conf.js
@@ -3,6 +3,14 @@ exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['dummy.spec.js'],
 
+    capabilities: {
+        'directConnect': true,
+        'browserName': 'chrome',
+        chromeOptions: {
+            args: ["--headless", "--disable-gpu", "--window-size=1920x1080"]
+        }
+    },
+
     onPrepare: function() {
         // Add a screenshot reporter and store screenshots to `/tmp/screenshots`:
         jasmine.getEnv().addReporter(new reporter())
